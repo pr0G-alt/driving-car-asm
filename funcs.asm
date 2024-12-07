baff2467a203052d99505756819cdb7934022362
pr0.G <pr0.G@outlook.com>
2024-12-07 18:48:42 +0200

    updated car movement

--- funcs.asm
+++ funcs.asm
@@ -2,6 +2,9 @@ init proc
     mov ax, width
     add ax, posX
     mov rowStop, ax
+    mov ax, height
+    add ax, posY
+    mov colStop, ax
     mov ax, width
     mov bx, height
     mul bx
@@ -28,8 +31,86 @@ drawLoop:
 nextLine:
     mov cx, posX
     inc dx
-    cmp si, area
+    cmp dx, colStop
     jne drawLoop
-    
+
     ret
 draw endp
+
+clearDown proc
+    mov cx, posX
+    mov dx, colStop
+    dec dx
+    mov ah, 0ch
+clearDownLoop:
+    mov al, 5
+    int 10h
+    inc cx
+    cmp cx, rowStop
+    jl clearDownLoop
+    
+    ret
+clearDown endp
+
+clearUp proc
+    mov cx, posX
+    mov dx, posY
+    mov ah, 0ch
+clearUpLoop:
+    mov al, 5
+    int 10h
+    inc cx
+    cmp cx, rowStop
+    jl clearUpLoop
+    
+    ret
+clearUp endp
+
+clearRight proc
+    dec rowStop
+    mov cx, rowStop
+    mov dx, posY
+    mov ah, 0ch
+clearRightLoop:
+    mov al, 5
+    int 10h
+    inc dx
+    cmp dx, colStop
+    jl clearRightLoop
+    
+    ret
+clearRight endp
+
+clearLeft proc
+    mov cx, posX
+    mov dx, posY
+    mov ah, 0ch
+clearLeftLoop:
+    mov al, 5
+    int 10h
+    inc dx
+    cmp dx, colStop
+    jl clearLeftLoop
+    
+    ret
+clearLeft endp
+
+background proc
+    mov ah, 0ch
+bgLoop:
+    mov al, 5
+    int 10h
+    inc cx
+    cmp cx, 320
+    je bgNextLine
+    jmp bgLoop
+    
+bgNextLine:
+    mov cx, 0
+    inc dx
+    cmp dx, 200
+    jne bgLoop
+
+    ret
+    
+background endp
--- main.asm
+++ main.asm
@@ -9,6 +9,7 @@ include imgData\car.asm
     width dw ?
     height dw ?
     rowStop dw ?
+    colStop dw ?
     area dw ?
     
 .code
@@ -22,15 +23,80 @@ main proc
     mov al, 13h
     int 10h
     
-car:
-    lea si, carData
+    mov cx, 0
+    mov dx, 0
+    call background
+   
     mov posX, 150
     mov posY, 79
+    
+drawCar:
+    lea si, carData
     mov width, 20
     mov height, 42
     call init
     call draw
+    
+getInput:
+    mov ah, 1h
+    int 16h
+    jz getInput
 
+    mov ah, 0h
+    int 16h
+
+    cmp ah, 1h
+    je exitGame
+
+    cmp ah, 48h
+    je moveUp
+    
+    cmp ah, 50h
+    je moveDown
+    
+    cmp ah, 4Bh
+    je moveLeft
+    
+    cmp ah, 4Dh
+    je moveRight
+
+    jmp getInput
+
+moveUp:
+    cmp posY, 0
+    je getInput
+    call clearDown
+    dec posY
+    jmp drawCar
+
+moveDown:
+    cmp posY, 200-42
+    je getInput
+    call clearUp
+    inc posY
+    jmp drawCar
+
+moveLeft:
+    cmp posX, 0
+    je getInput
+    call clearRight
+    dec posX
+    jmp drawCar
+
+moveRight:
+    cmp posX, 320-20
+    je getInput
+    call clearLeft
+    inc posX
+    jmp drawCar
+
+exitGame:
+    mov ax, 03h
+    int 10h
+
+    mov ax, 4C00h
+    int 21h
+    
 
 main endp
 
