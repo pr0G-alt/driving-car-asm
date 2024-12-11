2febf3cb925a039ae06d68e76fcf844de9d1aaa8
pr0.G <pr0.G@outlook.com>
2024-12-11 14:22:49 +0200

    organized files, drew bg with less data

--- funcs.asm
+++ funcs.asm
@@ -1,140 +1,110 @@
 ; background = draw background
-; init = initialize sprite data
-; draw = draw sprite
-; clearDown = clear upper row when moving down
-; clearUp = clear lower row when moving up
-; clearRight = clear right column when moving left
-; clearLeft = clear left row when moving right
+; car = draw car
 
 
 background proc
-    mov posX, 0
-    mov posY, 0
-    lea si, bgData
-    mov width, 320
-    mov height, 200
-    call init
-    call draw
+    mov cx, 0
+    mov dx, 0
+    
+    mov ah, 0ch
+    
+    mov al, 2
+drawBgLoop:
+    int 10h
+    inc cx
+    cmp cx, 90
+    jl drawBgLoop
+    mov al, 15
+    cmp cx, 95
+    jl drawBgLoop
+    mov al, 29
+    cmp cx, 97
+    jl drawBgLoop
+    mov al, 23
+    cmp cx, 320-97
+    jl drawBgLoop
+    mov al, 29
+    cmp cx, 320-95
+    jl drawBgLoop
+    mov al, 15
+    cmp cx, 320-90
+    jl drawBgLoop
+    mov al, 2
+    cmp cx, 320
+    jl drawBgLoop
+    
+    mov cx, 0
+    inc dx
+    cmp dx, 200
+    jl drawBgLoop
     
     ret
 background endp
 
-init proc
-    mov ax, width
-    add ax, posX
-    mov rowStop, ax
-    mov ax, height
-    add ax, posY
-    mov colStop, ax
-    mov ax, width
-    mov bx, height
-    mul bx
-    mov area, ax
-    mov cx, posX
-    mov dx, posY
+car proc
+    mov posX, 150
+    mov posY, 140
+drawCar:
+    lea si, carData
+    mov width, 20
+    mov height, 42
+    call init
+    call draw
     
-    ret
-init endp
+getInput:
+    mov ah, 1h
+    int 16h
+    jz getInput
 
-draw proc
-    mov ah, 0ch
+    mov ah, 0h
+    int 16h
+
+    cmp ah, 1h
+    je exitGame
+
+    cmp ah, 48h
+    je moveUp
     
-drawLoop:
-    mov al, [si]
-    int 10h
-
-    inc si
-    inc cx
-    cmp cx, rowStop
-    je nextLine
-    jmp drawLoop
+    cmp ah, 50h
+    je moveDown
     
-nextLine:
-    mov cx, posX
-    inc dx
-    cmp dx, colStop
-    jne drawLoop
-
-    ret
-draw endp
-
-clearDown proc
-    mov dx, colStop
-    mov bx, 0
-clearDown2:
-    mov cx, posX
-    dec dx
-    mov ah, 0ch
-clearDownLoop:
-    mov al, 23
-    int 10h
-    inc cx
-    cmp cx, rowStop
-    jl clearDownLoop
-    inc bx
-    cmp bx, 2
-    jl clearDown2
+    cmp ah, 4Bh
+    je moveLeft
     
-    ret
-clearDown endp
+    cmp ah, 4Dh
+    je moveRight
 
-clearUp proc
-    mov bx, 0
-    mov cx, posX
-    mov dx, posY
-clearUp2:
-    mov ah, 0ch
-clearUpLoop:
-    mov al, 23
-    int 10h
-    inc cx
-    cmp cx, rowStop
-    jl clearUpLoop
-    inc dx
-    mov cx, posX
-    inc bx
-    cmp bx, 2
-    jl clearUp2
-    ret
-clearUp endp
+    jmp getInput
 
-clearRight proc
-    dec rowStop
-    mov cx, rowStop
-    mov bx, 0
-clearRight2:
-    mov dx, posY
-    mov ah, 0ch
-clearRightLoop:
-    mov al, 23
-    int 10h
-    inc dx
-    cmp dx, colStop
-    jl clearRightLoop
-    dec cx
-    inc bx
-    cmp bx, 2
-    jl clearRight2
-    
-    ret
-clearRight endp
+moveUp:
+    cmp posY, 0
+    jle getInput
+    call clearDown
+    sub posY, 2
+    jmp drawCar
 
-clearLeft proc
-    mov cx, posX
-    mov bx, 0
-clearLeft2:
-    mov dx, posY
-    mov ah, 0ch
-clearLeftLoop:
-    mov al, 23
-    int 10h
-    inc dx
-    cmp dx, colStop
-    jl clearLeftLoop
-    inc cx
-    inc bx
-    cmp bx, 2
-    jl clearLeft2
-    
-    ret
-clearLeft endp
+moveDown:
+    cmp posY, 200-42
+    jge getInput
+    call clearUp
+    add posY, 2
+    jmp drawCar
+
+moveLeft:
+    cmp posX, 98
+    jle getInput
+    call clearRight
+    sub posX, 2
+    jmp drawCar
+
+moveRight:
+    cmp posX, 320-20-98
+    jge getInput
+    call clearLeft
+    add posX, 2
+    jmp drawCar
+
+exitGame:
+    mov ax, 4C00h
+    int 21h
+car endp
--- main.asm
+++ main.asm
@@ -3,7 +3,6 @@
 
 
 .data
-    include .\imgData\bg.asm
     include .\imgData\car.asm
     posX dw ?
     posY dw ?
@@ -11,10 +10,10 @@
     height dw ?
     rowStop dw ?
     colStop dw ?
-    area dw ?
     
 .code
 
+include .\preFuncs.asm
 include .\funcs.asm
 
 main proc
@@ -26,73 +25,8 @@ main proc
     
     call background
    
-    mov posX, 150
-    mov posY, 140
-    
-drawCar:
-    lea si, carData
-    mov width, 20
-    mov height, 42
-    call init
-    call draw
-    
-getInput:
-    mov ah, 1h
-    int 16h
-    jz getInput
+    call car
 
-    mov ah, 0h
-    int 16h
-
-    cmp ah, 1h
-    je exitGame
-
-    cmp ah, 48h
-    je moveUp
-    
-    cmp ah, 50h
-    je moveDown
-    
-    cmp ah, 4Bh
-    je moveLeft
-    
-    cmp ah, 4Dh
-    je moveRight
-
-    jmp getInput
-
-moveUp:
-    cmp posY, 0
-    jle getInput
-    call clearDown
-    sub posY, 2
-    jmp drawCar
-
-moveDown:
-    cmp posY, 200-42
-    jge getInput
-    call clearUp
-    add posY, 2
-    jmp drawCar
-
-moveLeft:
-    cmp posX, 98
-    jle getInput
-    call clearRight
-    sub posX, 2
-    jmp drawCar
-
-moveRight:
-    cmp posX, 320-20-98
-    jge getInput
-    call clearLeft
-    add posX, 2
-    jmp drawCar
-
-exitGame:
-    mov ax, 4C00h
-    int 21h
-    
 
 main endp
 
